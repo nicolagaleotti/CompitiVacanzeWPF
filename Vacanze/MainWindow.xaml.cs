1bf259d9f29c1a820dcba7fc5aa3fc0b1d4d16c2
Nicola Galeotti <nicola.galeotti9@gmail.com>
2020-01-04 20:12:43 +0100

    fine

--- Vacanze/MainWindow.xaml.cs
+++ Vacanze/MainWindow.xaml.cs
@@ -24,5 +24,31 @@ namespace Vacanze
         {
             InitializeComponent();
         }
+
+        private void Btn_Click(object sender, RoutedEventArgs e)
+        {
+            int a = int.Parse(txta.Text);
+            lbla.Content = $"a+1 = {a + 1}";
+            int b = int.Parse(txtb.Text);
+            lblb.Content = $"binario = {Convert.ToString(b, 2)}";
+            lblc.Content = $"binario = {Convert.ToString(-b, 2)}";
+            int value = int.Parse(txtvalue.Text);
+            lblvalue.Content = $"~value+1 = {~value + 1}; somma = {value + (~value + 1)}";
+            long number = long.Parse(txta.Text);
+            lblnumber.Content = $"{Convert.ToString(number, 2)}";
+        }
+
+        private void Txtb_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            int b = int.Parse(txtb.Text);
+            txtc.Text = (-b).ToString();
+        }
+
+        private void Txtnumber_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            long number = long.Parse(txtnumber.Text);
+            short number1 = (short)number;
+            txtnumber1.Text = number1.ToString();
+        }
     }
 }
